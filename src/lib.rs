a69b3a3280b6c635167dfe5c77c9f792a34335bf
Alex Gievsky <alex.gievsky@gmail.com>
2024-05-17 17:00:41 +0200

    Rename the output target, remove unused files.

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,30 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
-    a + b
+	a + b
+}
+
+#[wasm_bindgen]
+pub fn sub(a: i32, b: i32) -> i32 {
+	a - b
+}
+
+#[wasm_bindgen]
+pub fn pow2(p: i32) -> i32 {
+	2 << p
+}
+
+#[wasm_bindgen]
+pub fn div(a: i32, b: i32) -> i32 {
+	a / b
 }
 
 #[wasm_bindgen]
 pub fn mul(a: i32, b: i32) -> i32 {
-    a * b
+	a * b
 }
 
 #[test]
 fn add_test() {
-    assert_eq!(1 + 1, add(1, 1));
+	assert_eq!(1 + 1, add(1, 1));
 }
