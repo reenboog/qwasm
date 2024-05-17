38bcb4072b563c2db29f61b6e557eea8f373d93b
Alex Gievsky <alex.gievsky@gmail.com>
2024-05-17 16:25:44 +0200

    Initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[wasm_bindgen]
+pub fn mul(a: i32, b: i32) -> i32 {
+    a * b
+}
+
+#[test]
+fn add_test() {
+    assert_eq!(1 + 1, add(1, 1));
+}
